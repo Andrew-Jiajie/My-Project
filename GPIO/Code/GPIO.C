815ff54b8580ebb89ef322c7993dc7c466b9334d
Maker-Andrew <maker-andrew@Maker-AndrewdeMacBook-Pro-4.local>
2018-01-08 19:26:52 +0800

    Set the play order: head play then body play

--- GPIO/Code/GPIO.C
+++ GPIO/Code/GPIO.C
@@ -23,6 +23,8 @@
 #define HIGH 1
 #define LOW 0
 
+#define PLAYING 1
+#define STOP 0
 
 void Delay_1ms(int time)
 {
@@ -62,7 +64,7 @@ u8 return_finished[]={0x7E,0x04,   0x3E,    0x00,0x00,0xEF};  //current music fi
 u8 return_state[]={0x7E,0x03,0x10,0x00,   //0(STOP) 1(PLAY) 2(PAUS) 3(FF) 4(FR)
                   0xEF,0x7E,0x02,0x00,0xEF}; //when music_state send, will return the value
 u8 state_str[12];
-u8 get_state()
+u8 get_result()
 {
     int result_num=0;
 	int timeout=250;
@@ -103,7 +105,7 @@ void Control_CMD(u8* cmd, u8 num){
   	int j;
     while(!set_correct && timeout--){
       	send_cmd(cmd, num);
-    	str_num = get_state();
+    	str_num = get_result();
 #if DEBUG
   		Send_Data_To_UART0(0xAA);
   		for(j=0; j<str_num; j++){
@@ -125,6 +127,25 @@ void Control_CMD(u8* cmd, u8 num){
 }
 
 
+int Get_Play_State()
+{
+  	u8 timeout=3;
+	int str_num;
+	int j;
+	
+  	while(timeout--){
+    	send_cmd(music_state, sizeof(music_state));
+    	str_num = get_result();
+    	for(j=0; j<str_num-3; j++){
+      	  if(state_str[j]==0x7e && state_str[j+2]==0x10){
+        	  return state_str[j+3];
+      	  }
+    	}
+		Delay_1ms(20);
+  	}
+	return -1;
+}
+
 void Specify_Volume(u8 num)
 {
 	
@@ -132,60 +153,50 @@ void Specify_Volume(u8 num)
 	volume_set[1] = num;
 	Control_CMD(volume_set, sizeof(volume_set));
 #else
-  int set_correct=0;
-  u8 timeout=3;
+  	int set_correct=0;
+  	u8 timeout=3;
 	int str_num;
 	int j;
 	volume_set[1] = num;
-  while(!set_correct && timeout--){
-    send_cmd(volume_set, sizeof(volume_set));
-	Delay_1ms(13);	// 13ms is the best timedelay for volume set!
-    send_cmd(volume_get, sizeof(volume_get));
-    str_num = get_state();
+	
+  	while(!set_correct && timeout--){
+    	send_cmd(volume_set, sizeof(volume_set));
+		Delay_1ms(13);	// 13ms is the best timedelay for volume set!
+    	send_cmd(volume_get, sizeof(volume_get));
+    	str_num = get_result();
 #if DEBUG
-	Send_Data_To_UART0(0xAA);
-	for(j=0; j<str_num; j++){
-		Send_Data_To_UART0(state_str[j]);
-	}
-	Send_Data_To_UART0(0xBB);
+		Send_Data_To_UART0(0xAA);
+		for(j=0; j<str_num; j++){
+			Send_Data_To_UART0(state_str[j]);
+		}
+		Send_Data_To_UART0(0xBB);
 #endif
-    for(j=0; j<str_num-3; j++){
-      if(state_str[j]==0x7e && state_str[j+2]==0x11 && state_str[j+3]==num){
-        set_correct=1;
+    	for(j=0; j<str_num-3; j++){
+      	  if(state_str[j]==0x7e && state_str[j+2]==0x11 && state_str[j+3]==num){
+        	  set_correct=1;
 #if DEBUG
-		Send_Data_To_UART0(0xCC);
+			  Send_Data_To_UART0(0xCC);
 #endif
-        break;
-      }
-    }
-  }
+        	  break;
+      	  }
+    	}
+  	}
 #endif
 }
 
 int trig_state=HIGH;
-void set_trig_state(){
-	if(trig_state==0){
-		Enable_BIT7_HighLevel_Trig;
-		trig_state=1;
-	}else if(trig_state==1){
-		Enable_BIT7_LowLevel_Trig;
-		trig_state=0;
-	}
-}
-
 int Button_state=-1;
 void PinInterrupt_ISR (void) interrupt 7
 {
 	if (PIF == 0x80)
 	{
 		PIF =0;
-		Delay_1ms(15);
+		Delay_1ms(20);
 		if(trig_state==LOW && P17==LOW){
 			Enable_BIT7_RasingEdge_Trig;
 			trig_state=HIGH;
 			Button_state=1;
-		}
-		if(trig_state==HIGH && P17==HIGH){
+		}else if(trig_state==HIGH && P17==HIGH){
 			Enable_BIT7_FallEdge_Trig;
 			trig_state=LOW;
 			Button_state=0;
@@ -200,75 +211,61 @@ void EXT_INT0 (void) interrupt 0
 	Send_Data_To_UART0(0xAA);
 }
 
+int Head_Music_Play=0;
+int Body_Music_Play=0;
 void main (void) 
 {
-	int  Pin_last_state;
 	int music_num=1;
 	Set_All_GPIO_Quasi_Mode;					// Define in Function_define.h
 	
-#if 1
 	InitialUART0_Timer1(9600);
 	set_CLOEN; 
 	Delay_1ms(50);
-	//Specify_Volume(15);
-	//send_cmd(music_play, sizeof(music_play));
+
 #if 1
-P17_Input_Mode;
-set_P0S_7;
-Enable_INT_Port1;
-Enable_BIT7_FallEdge_Trig;
-trig_state=LOW;
-//set_trig_state();
+	P17_Input_Mode;
+	set_P0S_7;
+	Enable_INT_Port1;
+	Enable_BIT7_FallEdge_Trig;
+	trig_state=LOW;
 
-P30_Input_Mode;
-Enable_BIT0_FallEdge_Trig;
-//Enable_INT_Port3;
+	P30_Input_Mode;
+	Enable_BIT0_FallEdge_Trig;
+	//Enable_INT_Port3;
 
-set_EPI;							// Enable pin interrupt
-set_EX0;
-set_EA;								// global enable bit
+	set_EPI;							// Enable pin interrupt
+	set_EX0;
+	set_EA;								// global enable bit
 
 #endif
-	
-	Pin_last_state=P17;
+	Specify_Volume(15);
 	//Control_CMD(chip_sleep,sizeof(chip_sleep));
 	while(1){
-		Delay_1ms(15);
-		Send_Data_To_UART0(P17);
-		#if 0
-		if(Pin_last_state==1 && P17==0){
-			Button_state=1;
-		}
-		if(Pin_last_state==0 && P17==1){
-			Button_state=0;
-		}
-		#endif
-		Pin_last_state=P17;
 		Button_state=-1;
-		set_PD;
+		if(Head_Music_Play && Get_Play_State()==PLAYING){
+			Delay_1ms(50);
+		}else if(Body_Music_Play){
+			Control_CMD(folder_and_num, sizeof(folder_and_num));
+			Head_Music_Play=0;
+			Body_Music_Play=0;
+		}else{
+			Head_Music_Play=0;
+			set_PD;
+		}
 		if(Button_state==1){
-			//println("pressed");
 			folder_and_num[1]=1;
 			folder_and_num[2]=0;
 			Control_CMD(folder_and_num, sizeof(folder_and_num));
-			//send_cmd(music_next, sizeof(music_next));
-			Specify_Volume(40);
-		}
-		if(Button_state==0){
-			//println("released");
+			Head_Music_Play=1;
+		}else if(Button_state==0){
 			folder_and_num[1]=1;
-			folder_and_num[2]=music_num;
-			music_num++;
-			Control_CMD(folder_and_num, sizeof(folder_and_num));
-			//send_cmd(music_next, sizeof(music_next));
-			Specify_Volume(40);
+			folder_and_num[2]=music_num++;
+			Body_Music_Play=1;
 			if(music_num>12){
 				music_num=1;
 			}
-
 		}
 	}
-#endif
 }
 
 
