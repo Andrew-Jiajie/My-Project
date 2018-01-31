03162fd016570d6d0bd6888d56a2a56df2018465
Maker-Andrew <maker-andrew@Maker-AndrewdeMacBook-Pro-4.local>
2018-01-31 17:35:57 +0800

    lighted up the RGB light

--- GPIO/Code/GPIO.C
+++ GPIO/Code/GPIO.C
@@ -19,6 +19,9 @@
 #include "Common.h"
 #include "Delay.h"
 
+#define uchar unsigned char
+#define uint unsigned int
+
 #define DEBUG 0
 #define HIGH 1
 #define LOW 0
@@ -327,11 +330,256 @@ void audio_power_off()
 	set_PD;
 	set_TR0;                                    //Timer0 run
 }
+//-----------------------------------------------------------------------------------------------
+char SIN_TAB[64] = { 0x00, 0x0c, 0x18, 0x24, 0x30, 0x3b, 0x46, 0x50, 
+0x59, 0x62, 0x69, 0x70, 0x75, 0x79, 0x7c, 0x7e, 
+0x7f, 0x7e, 0x7c, 0x79, 0x75, 0x70, 0x69, 0x62, 
+0x59, 0x50, 0x46, 0x3b, 0x30, 0x24, 0x18, 0x0c, 
+0x00, -0x0c, -0x18, -0x24, -0x30, -0x3b, -0x46, -0x50, 
+-0x59, -0x62, -0x69, -0x70, -0x75, -0x79, -0x7c, -0x7e, 
+-0x7f, -0x7e, -0x7c, -0x79, -0x75, -0x70, -0x69, -0x62, 
+-0x59, -0x50, -0x46, -0x3b, -0x30, -0x24, -0x18, -0x0c, 
+ };
+//放大128倍后的cos整数表（128）
+char COS_TAB[64] = { 0x7f, 0x7e, 0x7c, 0x79, 0x75, 0x70, 0x69, 0x62, 
+0x59, 0x50, 0x46, 0x3b, 0x30, 0x24, 0x18, 0x0c, 
+0x00, -0x0c, -0x18, -0x24, -0x30, -0x3b, -0x46, -0x50, 
+-0x59, -0x62, -0x69, -0x70, -0x75, -0x79, -0x7c, -0x7e, 
+-0x7f, -0x7e, -0x7c, -0x79, -0x75, -0x70, -0x69, -0x62, 
+-0x59, -0x50, -0x46, -0x3b, -0x30, -0x24, -0x18, -0x0c, 
+0x00, 0x0c, 0x18, 0x24, 0x30, 0x3b, 0x46, 0x50, 
+0x59, 0x62, 0x69, 0x70, 0x75, 0x79, 0x7c, 0x7e, 
+ };
 
-void init_LED(){
-	
+//采样存储序列表
+char LIST_TAB[64] = { 0, 32, 16, 48, 8, 40, 24, 56,
+4, 36, 20, 52, 12, 44, 28, 60,
+2, 34, 18, 50, 10, 42, 26, 58,
+6, 38, 22, 54, 14, 46, 30, 62,
+1, 33, 17, 49, 9, 41, 25, 57,
+5, 37, 21, 53, 13, 45, 29, 61,
+3, 35, 19, 51, 11, 43, 27, 59,
+7, 39, 23, 55, 15, 47, 31, 63
+};
+
+uchar COUNT=15,COUNT1=0,ADC_Count=0,LINE=15,G,T;
+uchar i,j,k,b,p;                 
+int Temp_Real,Temp_Imag,temp;                // 中间临时变量  
+uint TEMP1;
+uchar PWM;  
+int Fft_Real[64]={0XFF}; 
+int Fft_Image[64];               // fft的虚部 
+uchar LED_TAB2[32];        //记录 漂浮物 是否需要 停顿一下
+uchar LED_TAB[32];       //记录红色柱状 
+uchar LED_TAB1[32];        //记录 漂浮点
+
+void FFT()
+{     
+  //uchar x;              
+    for( i=1; i<=6; i++)                            /* for(1) */
+    { 
+        b=1;
+        b <<=(i-1);                                       //碟式运算，用于计算 隔多少行计算 例如 第一极 1和2行计算，，第二级 
+        for( j=0; j<=b-1; j++)                              /* for (2) */
+        { 
+            p=1;
+            p <<= (7-i);            
+            p = p*j;
+            for( k=j; k<64; k=k+2*b)                /* for (3) 基二fft */
+            { 
+                Temp_Real = Fft_Real[k]; 
+        Temp_Imag = Fft_Image[k]; 
+        temp = Fft_Real[k+b];
+                Fft_Real[k] = Fft_Real[k] + ((Fft_Real[k+b]*COS_TAB[p])>>7) + ((Fft_Image[k+b]*SIN_TAB[p])>>7);
+                Fft_Image[k] = Fft_Image[k] - ((Fft_Real[k+b]*SIN_TAB[p])>>7) + ((Fft_Image[k+b]*COS_TAB[p])>>7);
+                Fft_Real[k+b] = Temp_Real - ((Fft_Real[k+b]*COS_TAB[p])>>7) - ((Fft_Image[k+b]*SIN_TAB[p])>>7);
+                Fft_Image[k+b] = Temp_Imag + ((temp*SIN_TAB[p])>>7) - ((Fft_Image[k+b]*COS_TAB[p])>>7);     
+                // 移位.防止溢出. 结果已经是本值的 1/64               
+                Fft_Real[k] >>= 1;             
+                Fft_Image[k] >>= 1; 
+                Fft_Real[k+b]  >>= 1;                 
+                Fft_Image[k+b]  >>= 1; 
+
+                                                                               
+            }     
+        } 
+    } 
+  Fft_Real[0]=Fft_Image[0]=0;     //去掉直流分量
+  //Fft_Real[63]=Fft_Image[63]=0;
+   // if(fractional_frequency==64)
+//  j_value=64;
+//  else
+//  j_value=20;
+#if 0
+    for(j=0; j<16; j++)
+    {
+     TEMP1=((((Fft_Real[j]* Fft_Real[j]))+((Fft_Image[j]*Fft_Image[j]))));//求功率
+    
+           if(TEMP1<9)i=1;             //求模并量化
+    else if(TEMP1<10)i=1;
+    else if(TEMP1<20)i=2;
+    else if(TEMP1<30)i=3;
+    else if(TEMP1<40)i=4;
+    else if(TEMP1<60)i=5;
+    else if(TEMP1<80)i=6;
+    else if(TEMP1<100)i=7;
+    else if(TEMP1<120)i=8;
+    else if(TEMP1<140)i=9;
+    else if(TEMP1<160)i=10;
+    else if(TEMP1<196)i=11;
+    else if(TEMP1<220)i=12;
+    else if(TEMP1<250)i=13;
+    else i=20;
+    
+    LED_TAB[j]=i; 
+
+  }       
+#else
+    for(j=0;j<17;j++) 
+  {
+    TEMP1=((((Fft_Real[j]* Fft_Real[j]))+((Fft_Image[j]*Fft_Image[j]))));//求功率
+  
+        if(TEMP1<6)
+      TEMP1=0;
+        else if(TEMP1<10)
+      TEMP1=1;
+        else if(TEMP1<16)
+      TEMP1=2;
+        else if(TEMP1<25)
+      TEMP1=3;
+        else if(TEMP1<36)
+      TEMP1=4;
+        else if(TEMP1<49)
+      TEMP1=5;
+        else if(TEMP1<55)
+      TEMP1=6;
+        else if(TEMP1<60)
+      TEMP1=7;
+        else if(TEMP1<65)
+      TEMP1=8;
+        else if(TEMP1<70)
+      TEMP1=9;
+        else if(TEMP1<75)
+      TEMP1=10;
+        else if(TEMP1<80)
+      TEMP1=11;
+        else if(TEMP1<96)
+      TEMP1=12;
+        else if(TEMP1<125)
+      TEMP1=13;
+        else if(TEMP1<156)
+      TEMP1=14;
+        else if(TEMP1<189)
+      TEMP1=15;
+        else if(TEMP1<224)
+      TEMP1=16;
+        else if(TEMP1<261)
+      TEMP1=17;
+        else if(TEMP1<300)
+      TEMP1=18;
+        else if(TEMP1<341)
+      TEMP1=19;
+        else if(TEMP1<384)
+      TEMP1=20;
+        else if(TEMP1<429)
+      TEMP1=21;
+        else if(TEMP1<476)
+      TEMP1=22;
+        else if(TEMP1<525)
+      TEMP1=23 ;
+        else if(TEMP1<576)
+      TEMP1=24;
+        else if(TEMP1<629)
+      TEMP1=25;
+        else if(TEMP1<684)
+      TEMP1=26;
+        else if(TEMP1<741)
+      TEMP1=27;
+        else if(TEMP1<800)
+      TEMP1=28;
+        else if(TEMP1<861)
+      TEMP1=29;
+        else if(TEMP1<1024)
+      TEMP1=30;
+        else 
+      TEMP1=31;
+  
+      //if(TEMP1>(LED_TAB[j]))
+      LED_TAB[j]=TEMP1;        
+        if(TEMP1>(LED_TAB1[j]))
+        {   
+      LED_TAB1[j]=TEMP1;
+            LED_TAB2[j]=7;  //12                                              //提顿速度=12
+        }
+  }
+  #endif
+}
+ void ADC_Finish()
+{
+    int ADC_Count=0;
+	Enable_ADC_AIN5;
+    while(ADC_Count<=64)
+    {
+      Fft_Real[LIST_TAB[ADC_Count]]=get_adc()-256; //按LIST_TAB表里的顺序，进行存储 采样值,,
+      //  ADC_CONTR = ADC_POWER | ADC_SPEEDHH| ADC_START | channel;   // 为了采集负电压，采用 偏置采集。电压提高到1/2 vcc，，所以要减去256
+      ADC_Count++;
+    }
 }
 
+// led num range is 0~1024
+void LED_R(int num){
+	PWM1H = HIBYTE(num);				
+	PWM1L = LOBYTE(num);
+	PWM1_OUTPUT_INVERSE;
+	set_LOAD;
+}
+void LED_G(int num){
+	PWM3H = HIBYTE(num);				
+	PWM3L = LOBYTE(num);
+	PWM3_OUTPUT_INVERSE;
+	set_LOAD;
+}
+void LED_B(int num){
+	PWM0H = HIBYTE(num);				
+	PWM0L = LOBYTE(num);
+	PWM0_OUTPUT_INVERSE;
+	set_LOAD;
+}
+void init_LED(){
+	PWM0_P12_OUTPUT_ENABLE;
+	PWM1_P14_OUTPUT_ENABLE;
+	PWM3_P00_OUTPUT_ENABLE;
+
+	PWM_IMDEPENDENT_MODE;
+	PWM_CLOCK_DIV_8;
+	PWMPH = 0x07;
+	PWMPL = 0xCF;
+	
+	LED_R(0);
+	LED_G(0);
+	LED_B(0);
+	set_PWMRUN;
+	while(0){
+		LED_R(1024);
+		Delay_1ms(500);
+		LED_R(0);
+		LED_G(1024);
+		Delay_1ms(500);
+		LED_G(0);
+		LED_B(1024);
+		Delay_1ms(500);
+		LED_B(0);
+	}
+}
+//-----------------------------------------------------------------------------------
+
+int get_adc()
+{
+	clr_ADCF;
+	set_ADCS;									// ADC start trig signal
+	while(ADCF == 0);
+  	return (int)(ADCRH<<2) + (int)((ADCRL&0x0f)>>2);//(((int)ADCRH)<<4+ADCRL&0x0f);
+}
 
 int Head_Music_Play=0;
 int Body_Music_Play=0;
@@ -343,6 +591,8 @@ void main (void)
 	//set_PD;									//powerdown directly 131.5uA
 	Set_All_GPIO_Quasi_Mode;					// Define in Function_define.h
 	P11_PushPull_Mode;
+	
+	
 	InitialUART0_Timer1(9600);
 	set_CLOEN; 
 	audio_power_on();
@@ -384,11 +634,11 @@ void main (void)
 	set_EA;								// global enable bit
 
 #endif
-	Specify_Volume(30);
+	init_LED();
+	Specify_Volume(20);
 	//Control_CMD(folder_and_num, sizeof(folder_and_num));
 	while(1){
 		//set_PD;					//powerdown here can be 145.8uA
-		//play_state=Get_Play_State();
 		if(Button_state==1){
 			Button_state=-1;
 			if(Power_state==OFF){
@@ -403,8 +653,36 @@ void main (void)
 			Body_Music_Play=1;
 		}
 		if(Play_state==PLAYING || Charge_state==ON){
+			int i=0;
+			int red=0, green=0, blue=0;
+			set_PWMRUN;
 			wake_time = 0;
 			//Send_Data_To_UART0(0xBB);
+			ADC_Finish();
+			FFT();
+#if 0
+		    for(i=0; i<16; i++){
+		      Send_Data_To_UART0(LED_TAB[i]);
+		    }
+			Send_Data_To_UART0('\n');
+#endif
+			red=LED_TAB[1]+LED_TAB[2]+LED_TAB[3]+LED_TAB[4]+LED_TAB[5];
+			green=LED_TAB[6]+LED_TAB[7]+LED_TAB[8]+LED_TAB[9]+LED_TAB[10];
+			blue=LED_TAB[11]+LED_TAB[12]+LED_TAB[13]+LED_TAB[14]+LED_TAB[15];
+			red=red*4;
+			green=green*4;
+			green=green*4;
+			LED_R(red);
+			LED_G(green);
+			LED_B(blue);
+		    
+			//set_IDL;
+		}
+		if(Play_state==STOP){
+			LED_R(0);
+			LED_G(0);
+			LED_B(0);
+			//clr_PWMRUN;
 			set_IDL;
 		}
 		if(Reset_system==1){
