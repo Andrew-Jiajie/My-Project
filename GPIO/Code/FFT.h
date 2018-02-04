0b2ed3f465d151973ef648c8e046a1dbffa0ea5b
Maker-Andrew <maker-andrew@Maker-AndrewdeMacBook-Pro-4.local>
2018-02-04 22:49:40 +0800

    split project into diffrent function files

---
+++ GPIO/Code/FFT.h
@@ -0,0 +1,184 @@
+//-----------------------------------------------------------------------------------------------
+char SIN_TAB[64] = { 0x00, 0x0c, 0x18, 0x24, 0x30, 0x3b, 0x46, 0x50, 
+0x59, 0x62, 0x69, 0x70, 0x75, 0x79, 0x7c, 0x7e, 
+0x7f, 0x7e, 0x7c, 0x79, 0x75, 0x70, 0x69, 0x62, 
+0x59, 0x50, 0x46, 0x3b, 0x30, 0x24, 0x18, 0x0c, 
+0x00, -0x0c, -0x18, -0x24, -0x30, -0x3b, -0x46, -0x50, 
+-0x59, -0x62, -0x69, -0x70, -0x75, -0x79, -0x7c, -0x7e, 
+-0x7f, -0x7e, -0x7c, -0x79, -0x75, -0x70, -0x69, -0x62, 
+-0x59, -0x50, -0x46, -0x3b, -0x30, -0x24, -0x18, -0x0c, 
+ };
+//放大128倍后的cos整数表（128）
+char COS_TAB[64] = { 0x7f, 0x7e, 0x7c, 0x79, 0x75, 0x70, 0x69, 0x62, 
+0x59, 0x50, 0x46, 0x3b, 0x30, 0x24, 0x18, 0x0c, 
+0x00, -0x0c, -0x18, -0x24, -0x30, -0x3b, -0x46, -0x50, 
+-0x59, -0x62, -0x69, -0x70, -0x75, -0x79, -0x7c, -0x7e, 
+-0x7f, -0x7e, -0x7c, -0x79, -0x75, -0x70, -0x69, -0x62, 
+-0x59, -0x50, -0x46, -0x3b, -0x30, -0x24, -0x18, -0x0c, 
+0x00, 0x0c, 0x18, 0x24, 0x30, 0x3b, 0x46, 0x50, 
+0x59, 0x62, 0x69, 0x70, 0x75, 0x79, 0x7c, 0x7e, 
+ };
+
+//采样存储序列表
+char LIST_TAB[64] = { 0, 32, 16, 48, 8, 40, 24, 56,
+4, 36, 20, 52, 12, 44, 28, 60,
+2, 34, 18, 50, 10, 42, 26, 58,
+6, 38, 22, 54, 14, 46, 30, 62,
+1, 33, 17, 49, 9, 41, 25, 57,
+5, 37, 21, 53, 13, 45, 29, 61,
+3, 35, 19, 51, 11, 43, 27, 59,
+7, 39, 23, 55, 15, 47, 31, 63
+};
+
+uchar COUNT=15,COUNT1=0,ADC_Count=0,LINE=15,G,T;
+uchar i,j,k,b,p;                 
+int Temp_Real,Temp_Imag,temp;                // 中间临时变量  
+uint TEMP1;
+uchar PWM;  
+int Fft_Real[64]={0XFF}; 
+int Fft_Image[64];               // fft的虚部 
+uchar LED_TAB2[32];        //记录 漂浮物 是否需要 停顿一下
+uchar LED_TAB[32];       //记录红色柱状 
+uchar LED_TAB1[32];        //记录 漂浮点
+
+void FFT()
+{     
+  //uchar x;              
+    for( i=1; i<=6; i++)                            /* for(1) */
+    { 
+        b=1;
+        b <<=(i-1);                                       //碟式运算，用于计算 隔多少行计算 例如 第一极 1和2行计算，，第二级 
+        for( j=0; j<=b-1; j++)                              /* for (2) */
+        { 
+            p=1;
+            p <<= (7-i);            
+            p = p*j;
+            for( k=j; k<64; k=k+2*b)                /* for (3) 基二fft */
+            { 
+                Temp_Real = Fft_Real[k]; 
+        Temp_Imag = Fft_Image[k]; 
+        temp = Fft_Real[k+b];
+                Fft_Real[k] = Fft_Real[k] + ((Fft_Real[k+b]*COS_TAB[p])>>7) + ((Fft_Image[k+b]*SIN_TAB[p])>>7);
+                Fft_Image[k] = Fft_Image[k] - ((Fft_Real[k+b]*SIN_TAB[p])>>7) + ((Fft_Image[k+b]*COS_TAB[p])>>7);
+                Fft_Real[k+b] = Temp_Real - ((Fft_Real[k+b]*COS_TAB[p])>>7) - ((Fft_Image[k+b]*SIN_TAB[p])>>7);
+                Fft_Image[k+b] = Temp_Imag + ((temp*SIN_TAB[p])>>7) - ((Fft_Image[k+b]*COS_TAB[p])>>7);     
+                // 移位.防止溢出. 结果已经是本值的 1/64               
+                Fft_Real[k] >>= 1;             
+                Fft_Image[k] >>= 1; 
+                Fft_Real[k+b]  >>= 1;                 
+                Fft_Image[k+b]  >>= 1; 
+
+                                                                               
+            }     
+        } 
+    } 
+  Fft_Real[0]=Fft_Image[0]=0;     //去掉直流分量
+  //Fft_Real[63]=Fft_Image[63]=0;
+   // if(fractional_frequency==64)
+//  j_value=64;
+//  else
+//  j_value=20;
+#if 0
+    for(j=0; j<16; j++)
+    {
+     TEMP1=((((Fft_Real[j]* Fft_Real[j]))+((Fft_Image[j]*Fft_Image[j]))));//求功率
+    
+           if(TEMP1<9)i=1;             //求模并量化
+    else if(TEMP1<10)i=1;
+    else if(TEMP1<20)i=2;
+    else if(TEMP1<30)i=3;
+    else if(TEMP1<40)i=4;
+    else if(TEMP1<60)i=5;
+    else if(TEMP1<80)i=6;
+    else if(TEMP1<100)i=7;
+    else if(TEMP1<120)i=8;
+    else if(TEMP1<140)i=9;
+    else if(TEMP1<160)i=10;
+    else if(TEMP1<196)i=11;
+    else if(TEMP1<220)i=12;
+    else if(TEMP1<250)i=13;
+    else i=20;
+    
+    LED_TAB[j]=i; 
+
+  }       
+#else
+    for(j=0;j<17;j++) 
+  {
+    TEMP1=((((Fft_Real[j]* Fft_Real[j]))+((Fft_Image[j]*Fft_Image[j]))));//求功率
+  
+        if(TEMP1<6)
+      TEMP1=0;
+        else if(TEMP1<10)
+      TEMP1=1;
+        else if(TEMP1<16)
+      TEMP1=2;
+        else if(TEMP1<25)
+      TEMP1=3;
+        else if(TEMP1<36)
+      TEMP1=4;
+        else if(TEMP1<49)
+      TEMP1=5;
+        else if(TEMP1<55)
+      TEMP1=6;
+        else if(TEMP1<60)
+      TEMP1=7;
+        else if(TEMP1<65)
+      TEMP1=8;
+        else if(TEMP1<70)
+      TEMP1=9;
+        else if(TEMP1<75)
+      TEMP1=10;
+        else if(TEMP1<80)
+      TEMP1=11;
+        else if(TEMP1<96)
+      TEMP1=12;
+        else if(TEMP1<125)
+      TEMP1=13;
+        else if(TEMP1<156)
+      TEMP1=14;
+        else if(TEMP1<189)
+      TEMP1=15;
+        else if(TEMP1<224)
+      TEMP1=16;
+        else if(TEMP1<261)
+      TEMP1=17;
+        else if(TEMP1<300)
+      TEMP1=18;
+        else if(TEMP1<341)
+      TEMP1=19;
+        else if(TEMP1<384)
+      TEMP1=20;
+        else if(TEMP1<429)
+      TEMP1=21;
+        else if(TEMP1<476)
+      TEMP1=22;
+        else if(TEMP1<525)
+      TEMP1=23 ;
+        else if(TEMP1<576)
+      TEMP1=24;
+        else if(TEMP1<629)
+      TEMP1=25;
+        else if(TEMP1<684)
+      TEMP1=26;
+        else if(TEMP1<741)
+      TEMP1=27;
+        else if(TEMP1<800)
+      TEMP1=28;
+        else if(TEMP1<861)
+      TEMP1=29;
+        else if(TEMP1<1024)
+      TEMP1=30;
+        else 
+      TEMP1=31;
+  
+      //if(TEMP1>(LED_TAB[j]))
+      LED_TAB[j]=TEMP1;        
+        if(TEMP1>(LED_TAB1[j]))
+        {   
+      LED_TAB1[j]=TEMP1;
+            LED_TAB2[j]=7;  //12                                              //提顿速度=12
+        }
+  }
+  #endif
+}
---
+++ GPIO/Code/Music_control.h
@@ -0,0 +1,171 @@
+
+#define HEAD_MUSIC_NUM 2
+
+uchar music_play[]={0x01};
+uchar music_stop[]={0x0E};
+uchar music_next[]={0x03};
+uchar volume_set[]={0x31,0x10};
+uchar source_TF[]={0x35,0x01};
+uchar source_FLASH[]={0x35,0x04};
+uchar music_for_head[]={0x42,0x01,0x02};
+uchar music_for_next[]={0x41,0x00,0x01};
+uchar music_in_root[]={0x41,0x00,0x01};
+uchar folder_and_num[]={0x42,0x01,0x01};
+uchar chip_sleep[]={0x35,0x03};
+uchar chip_wakeup[]={0x35,0x02};
+
+uchar music_state[]={0x10};
+uchar music_number[]={0x17};
+uchar volume_get[]={0x11};
+uchar current_source[]={0x18};
+
+uchar return_ok[]={0x7E,0x02,0x00,0xEF};
+uchar return_finished[]={0x7E,0x04,   0x3E,    0x00,0x00,0xEF};  //current music finished
+uchar return_state[]={0x7E,0x03,0x10,0x00,   //0(STOP) 1(PLAY) 2(PAUS) 3(FF) 4(FR)
+                  0xEF,0x7E,0x02,0x00,0xEF}; //when music_state send, will return the value
+uchar state_str[12];
+
+uchar get_result()
+{
+    int result_num=0;
+	int timeout=250;
+
+	while(!RI && timeout--){
+		Delay_100us(1);		//delay 25ms
+	}
+    while (RI) {
+      state_str[result_num++]=Receive_Data_From_UART0();
+	  timeout=8000;
+	  while(!RI && timeout--);
+    }
+    return result_num;
+}
+
+void send_cmd(uchar* cmd, uchar num){
+  uchar count=0;
+  uchar command[15];
+  uchar tmp_cnt=0;
+  int out_cnt=0;
+	
+  command[count++]=0x7E;
+  command[count++]=0x01+num;
+  while(tmp_cnt<num){
+    command[count++]=*(cmd+tmp_cnt);
+    tmp_cnt++;
+  }
+  command[count]=0xEF;
+  while(out_cnt<=count){
+    Send_Data_To_UART0(command[out_cnt++]);
+  }
+}
+
+int Control_CMD(uchar* cmd, uchar num){
+	int set_correct=0;
+    uchar timeout=2;
+  	int str_num;
+  	int j;
+    while(!set_correct && timeout--){
+      	send_cmd(cmd, num);
+    	str_num = get_result();
+      	for(j=0; j<str_num-3; j++){
+        	if(state_str[j]==0x7e && state_str[j+1]==0x02 && state_str[j+2]==0x00 && state_str[j+3]==0xef){
+          		set_correct=1;
+				return 0;
+          	  	break;
+        	}
+      	}
+    }
+	return -1;
+}
+
+
+int Get_Play_State()
+{
+  	uchar timeout=2;
+	int str_num;
+	int j;
+	
+  	while(timeout--){
+    	send_cmd(music_state, sizeof(music_state));
+    	str_num = get_result();
+    	for(j=0; j<str_num-3; j++){
+      	  if(state_str[j]==0x7e && state_str[j+2]==0x10){
+        	  return state_str[j+3];
+      	  }
+    	}
+		Delay_1ms(20);
+  	}
+	return -1;
+}
+
+int Get_total_number()
+{
+  	uchar timeout=2;
+	int str_num;
+	int j;
+	
+  	while(timeout--){
+    	send_cmd(music_number, sizeof(music_number));
+    	str_num = get_result();
+    	for(j=0; j<str_num-4; j++){
+      	  if(state_str[j]==0x7e && state_str[j+2]==0x17){
+			  return state_str[j+4];
+      	  }
+    	}
+		Delay_1ms(20);
+  	}
+	return -1;
+}
+
+int Play_head_music(){
+	Control_CMD(music_for_head, sizeof(music_for_head));
+	music_for_head[2]++;
+	if(LOBYTE(music_for_head[2])>HEAD_MUSIC_NUM){
+		music_for_head[2]=1;
+	}
+}
+
+int Play_body_music()
+{
+	int total_music=Get_total_number();
+	if(total_music > HEAD_MUSIC_NUM){
+		Control_CMD(music_for_next, sizeof(music_for_next));
+		music_for_next[2]++;
+		if(LOBYTE(music_for_next[2])>total_music-HEAD_MUSIC_NUM){
+			music_for_next[2]=1;
+		}
+	}
+}
+void Stop_music()
+{
+	Control_CMD(music_stop, sizeof(music_stop));
+}
+
+int Specify_Volume(uchar num)
+{
+	
+#if 0
+	volume_set[1] = num;
+	Control_CMD(volume_set, sizeof(volume_set));
+#else
+  	int set_correct=0;
+  	uchar timeout=3;
+	int str_num;
+	int j;
+	volume_set[1] = num;
+	
+  	while(!set_correct && timeout--){
+    	send_cmd(volume_set, sizeof(volume_set));
+		Delay_1ms(13);	// 13ms is the best timedelay for volume set!
+    	send_cmd(volume_get, sizeof(volume_get));
+    	str_num = get_result();
+    	for(j=0; j<str_num-3; j++){
+      	  if(state_str[j]==0x7e && state_str[j+2]==0x11 && state_str[j+3]==num){
+        	  set_correct=1;
+			  return 0;
+      	  }
+    	}
+  	}
+	return -1;
+#endif
+}
